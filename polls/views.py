c55db90a6994ad4fb983ff788856e45e247abc52
m02xime <maxkb02@gmail.com>
2023-10-25 14:58:11 +0200

    test

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel! test new</h1>
+            <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
